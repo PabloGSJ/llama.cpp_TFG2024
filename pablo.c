fa970cbfe29e5e394a81a67db6db162e160b1338
Pablo Gonzalez San Jose <pablogonsanj@gmail.com>
2024-03-26 14:36:46 +0100

    Solucionado error en pablo.c

--- pablo.c
+++ pablo.c
@@ -1,9 +1,9 @@
 #include "pablo.h"
 #include <stdio.h>
 
-pablo_tid = 0;
-pablo_rid = 0;
-pablo_histogram[PABLO_N_TENSORS][PABLO_TENSOR_SIDE][PABLO_MAX_HIST_VALUE] = {0};
+int pablo_tid = 0;
+int pablo_rid = 0;
+int pablo_histogram[PABLO_N_TENSORS][PABLO_TENSOR_SIDE][PABLO_MAX_HIST_VALUE] = {0};
 
 
 void pablo_print_row() {
