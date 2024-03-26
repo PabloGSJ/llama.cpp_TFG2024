f055dc718398640f73020eb104d212639537be0b
Pablo Gonzalez San Jose <pablogonsanj@gmail.com>
2024-03-26 11:25:09 +0100

    .

--- pablo.c
+++ pablo.c
@@ -1,12 +1,3 @@
 #include "pablo.h"
 #include <stdio.h>
 
-void print_quant_times(quant_times qt) {
-
-    printf("%s - %dth iter: -------------------------------------------------------------\n", PABLO_PREFIX, pablo_nq);
-    printf("%s - Total exec time:\t%f\n", PABLO_PREFIX, qt.total_time);
-    printf("%s - Time per block:\t\t%f\n", PABLO_PREFIX, qt.total_time);
-    printf("%s - Time to find max:\t%f\n", PABLO_PREFIX, qt.total_time);
-    printf("%s - Time per iter of j2:\t%f\n", PABLO_PREFIX, qt.total_time);
-    pablo_nq++;
-}
