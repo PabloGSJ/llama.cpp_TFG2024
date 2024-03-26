d682fec4618272868c55119fa4414518aa3e6efb
Pablo Gonzalez San Jose <pablogonsanj@gmail.com>
2024-03-26 12:45:41 +0100

    Cambiado el nombre de la constante PABLO_H

--- pablo.h
+++ pablo.h
@@ -1,14 +1,8 @@
 #pragma once
-#ifndef _PABLO_H
-#define _PABLO_H
+#ifndef PABLO_H
+#define PABLO_H
 
 #include <time.h>
-
-// DEBUG
-#define _MTOTAL
-#define _MBLOCK
-#define _MMAX
-#define _MJ2
 
 #define PABLO_PREFIX        "PABLO"
 #define PABLO_N_TENSORS     291
@@ -19,4 +13,4 @@ int pablo_histogram[PABLO_N_TENSORS][PABLO_TENSOR_SIDE][16] = {0};  // histogram
 int pablo_tid = 0;                                                  // active tensor id
 int pablo_rid = 0;                                                  // active row id
 
-#endif /*_PABLO_H*/
+#endif /*PABLO_H*/
