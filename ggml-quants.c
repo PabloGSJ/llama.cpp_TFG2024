a7a55c81c5b1f07511d6345d71e2a574a71bc14b
Pablo Gonzalez San Jose <pablogonsanj@gmail.com>
2024-07-05 11:07:03 +0200

    Arreglados parametros de dequantize_debug y quantize_debug

--- ggml-quants.c
+++ ggml-quants.c
@@ -1198,7 +1198,7 @@ void quantize_row_q8_1(const float * restrict x, void * restrict vy, int k) {
 }
 
 // PABLO:
-void dequantize_row_pablo(const block_q4_0 * restrict x, float * restrict y, int k) {
+void dequantize_row_pablo(const block_pablo * restrict x, float * restrict y, int k) {
     pablo_dequantize_row_assign(x, y, k);
     exit(0);
 }
