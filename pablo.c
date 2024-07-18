e9549ae5d928b8c4de4b66ec368a0c6efaf809f8
Pablo Gonzalez San Jose <pablogonsanj@gmail.com>
2024-07-18 17:14:47 +0200

    El error esta en pablo_quantize_row -> en el acceso a pablo_encoding_table. Probando soluciones

--- pablo.c
+++ pablo.c
@@ -155,11 +155,6 @@ void pablo_print_all(void) {    // json format
  */
 void pablo_quantize_row_assign(const float * restrict x, block_pablo * restrict y, int k) {
     fprintf(stderr, "\n\nPABLO: Entered pablo_quantize_row_assign\n");
-
-    for (int i = 0; i < 256; i++) {
-        fprintf(stderr, "%d-", pablo_encoding_table[i]);
-    }
-    fprintf(stderr, "\n");
     
     pablo_quantize_row(x, y, k);
 }
@@ -176,6 +171,18 @@ void pablo_quantize_row(const float * restrict x, block_pablo * restrict y, int 
     // translate to 16 bit values
     assert(k % PABLO == 0);
     const int nb = k / PABLO;
+
+    fprintf(stderr, "PABLO: Checking quantization...\n");
+    for (int i = 0; i < nb; i++) {
+        for (int j = 0; j < PABLO; ++j) {
+            
+            if (y[i].qs[j] < -128 || y[i].qs[j] >= 128) {
+                // ERROR:
+                fprintf("PABLO ERROR: quantization errors detected: y[%d].qs[%d]=%d\n", i, j, y[i].qs[j]);
+            }
+        }
+    }
+    fprintf(stderr, "PABLO: Quantization successful\n\n");
 
     fprintf(stderr, "PABLO: Begining translation...\n");
     int tmp;
