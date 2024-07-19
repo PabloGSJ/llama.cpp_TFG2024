1af432992b5854c24931291228bc4da7aa8ac623
Pablo Gonzalez San Jose <pablogonsanj@gmail.com>
2024-07-19 04:07:42 +0200

    El error esta en pablo_quantize_row -> en el acceso a pablo_encoding_table. Probando soluciones

--- pablo.c
+++ pablo.c
@@ -236,13 +236,42 @@ void pablo_quantize_debug(const float * restrict x, block_pablo * restrict y, in
     fprintf(stderr, "Translation successful\n\n");
 }
 
-
-
-// Dequantization functions
+// ##### Dequantization functions #################################################################################
+/**
+ * Select the appropriate pablo-dequantization function according to the operation mode
+ */
 void pablo_dequantize_row_assign(const block_pablo * restrict x, float * restrict y, int k) {
+    //pablo_dequantize_debug(x, y, k);
+    pablo_dequantize_row(x, y, k);
+}
+
+/**
+ * Main pablo-dequantization function
+ */
+void pablo_dequantize_row(const block_pablo * restrict x, float * restrict y, int k) {
     
-    // debug
-    printf("PABLO: alcanzado pablo_dequantize");
+    assert(k % PABLO == 0);
+    const int nb = k / PABLO;
+
+    for (int i = 0; i < nb; i++) {
+        for (int j = 0; j < PABLO; ++j) {
+
+            y[i*PABLO + j] = decoding_table[x[i].qs[j] + DECODING_OFFSET];
+        }
+    }
+}
+
+void pablo_dequantize_row_imprecise(const block_q4_0 * restrict x, float * restrict y, int k) {
+    // not implemented
+}
+
+/**
+ * Debug function for various purposes
+ */
+void pablo_dequantize_debug(const block_pablo * restrict x, float * restrict y, int k) {
+    // check that correct quantized tensors were received 
+
+    printf("PABLO: pablo_dequantize_debug reached\n");
 
     static const int qk = QK8_0;
 
@@ -254,56 +283,13 @@ void pablo_dequantize_row_assign(const block_pablo * restrict x, float * restric
         for (int j = 0; j < qk; ++j) {
             
             if (x[i].qs[j] != 123) {
-                printf("PABLO: Encontrada discrepancia:\n");
+                printf("PABLO: Found discrepancy:\n");
                 printf("PABLO: x[%d].qs[%d] = %d", i, j, x[i].qs[j]);
                 exit(-1);
             }
         }
     }
-    printf("PABLO: exito al comprobar!\n");
+    printf("PABLO: successful check!\n");
     exit(0);
 
-    #ifdef PABLO_PRECISION_QUANTIZATION
-        //pablo_dequantize_row(x, y, k);
-    #endif
-
-    #ifndef PABLO_PRECISION_QUANTIZATION
-        pablo_dequantize_row_imprecise(x, y, k);
-    #endif
-}
-
-void pablo_dequantize_row(const block_pablo * restrict x, float * restrict y, int k) {
-    static const int qk = QK8_0;
-
-    assert(k % qk == 0);
-
-    const int nb = k / qk;
-
-    for (int i = 0; i < nb; i++) {
-        const float d = GGML_FP16_TO_FP32(x[i].d);
-
-        for (int j = 0; j < qk; ++j) {
-            y[i*qk + j] = x[i].qs[j]*d;
-        }
-    }
-}
-
-void pablo_dequantize_row_imprecise(const block_q4_0 * restrict x, float * restrict y, int k) {
-    static const int qk = QK4_0;
-
-    assert(k % qk == 0);
-
-    const int nb = k / qk;
-
-    for (int i = 0; i < nb; i++) {
-        const float d = GGML_FP16_TO_FP32(x[i].d);
-
-        for (int j = 0; j < qk/2; ++j) {
-            const int x0 = (x[i].qs[j] & 0x0F) - 8;
-            const int x1 = (x[i].qs[j] >>   4) - 8;
-
-            y[i*qk + j + 0   ] = x0*d;
-            y[i*qk + j + qk/2] = x1*d;
-        }
-    }
 }
