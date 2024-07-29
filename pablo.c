491e8d9015b98cda08a2ae9a30f9613d5e679527
Pablo Gonzalez San Jose <pablogonsanj@gmail.com>
2024-07-29 14:29:57 +0200

    Implementando pablo_init

--- pablo.c
+++ pablo.c
@@ -86,7 +86,7 @@ int8_t basic_decoding_table[16] = {
 int8_t balanced_decoding_table[16] = {-127, -65, -50, -38, -28, -19, -10, -1, 0, 9, 18, 27, 37, 49, 64, 127};
 
 // pablo_q4_0 quantization radius
-unsigned int q4_0_radius = 0;
+int8_t q4_0_radius = 0;
 
 // encoding-decoding table set to use
 enum _table_mode {BASIC_TABLE, BALANCED_TABLE};
@@ -133,12 +133,15 @@ void pablo_init(void) {
     int table;
 
     fread(mode, sizeof(char), 4, fp);
-    fscanf(fp, "%u %d", &q4_0_radius, &table);
+    fscanf(fp, "%d %d", &q4_0_radius, &table);
 
     fclose(fp);
 
     // Initialize the rest of the data
     do_pablo = (strcmp(mode, "PBLO") == 0);
+
+    if (q4_0_radius < 0) 
+        q4_0_radius = 0;
 
     switch(table) {
 
