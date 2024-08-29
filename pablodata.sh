1f2f11e50ba5619940b389f28956b97939491c1c
Pablo <pgs179@alumnos.unican.es>
2024-08-29 20:13:18 +0200

    Tomar graficos

--- pablodata.sh
+++ pablodata.sh
@@ -14,7 +14,7 @@ sed -n '6p' pablodata/tmp.json > pablodata/pablo_r0-q4_0_data.json
 
 # pablo_q4_0 radius=[1, 2, 3]
 for i in 1 2 3 ; do
-
+echo PABLO $i
 echo PBLO $i 0 > pablo.conf
 ./quantize models/llama-2-7b/ggml-model-f16.gguf models/llama-2-7b/pablo_ignore Q4_0 1 > pablodata/tmp.json 
 sed -n '6p' pablodata/tmp.json > pablodata/pablo_r$i-q4_0_data.json
