a5df69e395ba0780eacb3732afb71e9c59683541
Pablo <pgs179@alumnos.unican.es>
2024-08-30 17:08:33 +0200

    Tomar graficos

--- pablodata.sh
+++ pablodata.sh
@@ -19,8 +19,8 @@ echo 0 0 0 > pablo.conf
 ./quantize models/llama-2-7b/ggml-model-f16.gguf models/llama-2-7b/pablo_ignore Q8_0 1 | sed -n '6p' > $HOME/json/simple_q8-0.json
 
 # pablo_q8_0 table=basic
-echo 1 0 0 > pablo.conf 
-./quantize models/llama-2-7b/ggml-model-f16.gguf models/llama-2-7b/pablo_ignore Q8_0 1 | sed -n '6p' > $HOME/json/pablo_q8-0_basict.json
+# echo 1 0 0 > pablo.conf 
+# ./quantize models/llama-2-7b/ggml-model-f16.gguf models/llama-2-7b/pablo_ignore Q8_0 1 | sed -n '6p' > $HOME/json/pablo_q8-0_basict.json
 
 # pablo_q8_0 table=balanced
 echo 1 0 1 > pablo.conf 
