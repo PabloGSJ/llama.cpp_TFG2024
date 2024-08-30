a451f24649c88569137dedc0169e2bd25842c40a
Pablo <pgs179@alumnos.unican.es>
2024-08-30 17:26:30 +0200

    Tomar graficos

--- pablodata.sh
+++ pablodata.sh
@@ -14,13 +14,13 @@ echo 1 $i 0 > pablo.conf
 ./quantize models/llama-2-7b/ggml-model-f16.gguf models/llama-2-7b/pablo_ignore Q4_0 1 | sed -n '6p' > $HOME/json/pablo_q4-0_r-$i.json
 done
 
-# simple_q8_0
-echo 0 0 0 > pablo.conf 
-./quantize models/llama-2-7b/ggml-model-f16.gguf models/llama-2-7b/pablo_ignore Q8_0 1 | sed -n '6p' > $HOME/json/simple_q8-0.json
+# # simple_q8_0
+# echo 0 0 0 > pablo.conf 
+# ./quantize models/llama-2-7b/ggml-model-f16.gguf models/llama-2-7b/pablo_ignore Q8_0 1 | sed -n '6p' > $HOME/json/simple_q8-0.json
 
-# pablo_q8_0 table=basic
-# echo 1 0 0 > pablo.conf 
-# ./quantize models/llama-2-7b/ggml-model-f16.gguf models/llama-2-7b/pablo_ignore Q8_0 1 | sed -n '6p' > $HOME/json/pablo_q8-0_basict.json
+pablo_q8_0 table=basic
+echo 1 0 0 > pablo.conf 
+./quantize models/llama-2-7b/ggml-model-f16.gguf models/llama-2-7b/pablo_ignore Q8_0 1 | sed -n '6p' > $HOME/json/pablo_q8-0_basict.json
 
 # pablo_q8_0 table=balanced
 echo 1 0 1 > pablo.conf 
